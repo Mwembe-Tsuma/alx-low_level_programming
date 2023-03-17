93e92e01cc05f82849c56029754f25cfece78dbd
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-17 17:39:00 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar('0' + num);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+}
