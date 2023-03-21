33abd513b7d531d6a7d799c458329b44eaa8c395
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-21 15:14:49 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add-Program thatadds two integers
+ *Return: Results of two intergers
+ *@b: First integer
+ *@a: Second integer
+ */
+
+int add(int a, int b)
+{
+	int results;
+
+	results = a + b;
+
+	return (results);
+}
