e436b5f00f953f061e38f2d9185850b59ccebf57
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-21 10:55:42 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main- Entry point
+ * print_alphabet- Entry point
  * Return: Always 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
-	return (0);
 }
