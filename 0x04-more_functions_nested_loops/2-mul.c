35871c9704831dcff14e4cad2368017acca476e5
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-23 09:37:40 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul- Function to multiply two intergers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: 0 success
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
