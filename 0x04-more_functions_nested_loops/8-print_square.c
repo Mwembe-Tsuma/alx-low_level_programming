51232b1ad9ffb0d8a8bca7feaf647f5986d323a3
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-23 14:36:59 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square- Print a square
+ * @size: The size of the square
+ *
+ * Return: 0 Success
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
