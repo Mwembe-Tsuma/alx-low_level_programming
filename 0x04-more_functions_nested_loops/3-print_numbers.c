0d789a4df81f4c56bc1544178890e0ac8e21b013
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-23 09:50:58 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers- Prints a lowercase alphabet
+ *
+ * Return: 0 success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
