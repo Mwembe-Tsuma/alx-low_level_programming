7c2d729f0ec2445698001ce4ed8598bec6c9f56b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-23 15:43:57 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,13 @@ int main(void)
 {
 	int n;
 
-	printf("\n");
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
-		else if(n % 5 == 0)
+		else if (n % 5 == 0)
 		{
 			printf("Buzz ");
 		}
