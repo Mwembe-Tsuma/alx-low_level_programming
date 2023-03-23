a88e2a9b49fe5baf4abd9233304c2d56b6ece514
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-23 09:28:08 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit- Prints a lowercase alphabet
+ *@c: Character to be checked
+ * Return: 1 if true and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
