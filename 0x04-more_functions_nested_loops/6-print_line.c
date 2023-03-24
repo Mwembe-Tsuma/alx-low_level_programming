c438fe1ae4e6028decccf73567eadc31021e9f84
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-24 09:48:45 +0000

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,17 +10,16 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		while (i <= n)
-		{
-			_putchar(95);
-			i++;
-		}
 		_putchar('\n');
 	}
 	else
 	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
 		_putchar('\n');
 	}
 }
