db53534da0b02275f9196326c579580d69221496
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-27 15:10:53 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev- Print a string in reverse
+ * @s: The strings to be printed
+ *
+ * Return: 0 Success
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while(*s++)
+
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(*s--);
+	_putchar('\n');
+}
