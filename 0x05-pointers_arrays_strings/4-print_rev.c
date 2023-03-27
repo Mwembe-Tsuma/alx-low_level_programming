3642313a28213d891451946c3d196ba6891f2237
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-27 15:25:47 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,14 @@ void print_rev(char *s)
 {
 	int len = 0, index;
 
-	while(*s++)
+	while (*s != '\0')
+	{
 
 		len++;
+		s++;
+	}
 
-	for (index = len - 1; index >= 0; index--)
+	for (index = len; index >= 0; index--)
 		_putchar(*s--);
 	_putchar('\n');
 }
