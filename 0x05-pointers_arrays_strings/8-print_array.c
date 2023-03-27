002945471f0baf7fe252c15ca23a30042354e1ab
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-27 18:25:53 +0000

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -18,7 +19,8 @@ void print_array(int *a, int n)
 	}
 	if (i == (n - 1))
 	{
-		printf("%d, ", a[n - 1]);
+		printf("%d", a[n - 1]);
 	}
 	printf("\n");
 }
+
