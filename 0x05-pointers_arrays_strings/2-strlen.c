d67125aa8fc13ef93407a983d7350d434cc22d6b
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-27 13:34:04 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen- Finds the lenght of a string
+ * @*s: The strings
+ *
+ * Return: 0 Success
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return i;
+}
