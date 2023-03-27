aa4dc712c57df4535246dfff280fed6d70be8e22
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-27 11:47:48 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int- Swap integers
+ *@a: Firts integer
+ *@b: Second integer
+ *
+ * Return: 0 success
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
