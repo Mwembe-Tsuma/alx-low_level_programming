864343b74e999891e5a34e4315a28accd6af7c9c
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-29 13:12:22 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13- Encoded a string using rot13
+ * @n: elements in array
+ *Return: encoded array
+ */
+char *rot13(char *n)
+{
+	int i, j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
+	}
+	return (n);
+}
