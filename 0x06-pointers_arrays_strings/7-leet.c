1432c9c31340e21b716d85467e5fdd9580a628d8
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-03-29 16:41:10 +0000

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,19 +7,20 @@
  */
 char *leet(char *n)
 {
-	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "2233007711";
+	char *r = n;
+	char s1[] = {'a', 'e', 'o', 't', 'l'};
+	char s2[] = {4, 3, 0, 7, 1};
+	int i = 0;
 
-	for (i = 0; n[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; j < 10; j++)
+		for (i = 0; i < 5; i++)
 		{
-			if (n[i] == s1[j])
+			if (*s == s1[i] || *s == s1[i] - 32)
 			{
-				n[i] = s2[j];
+				*s = s2[i] + '0';
 			}
 		}
 	}
-	return (n);
+	return (r);
 }
