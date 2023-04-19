cce3a0b6557fea6484977de9d4d6d05d6b365b6f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-04-19 14:52:39 +0000

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main- Main function
+  *@argc: argument count
+  *@argv: argument array
+  *
+  *Return: 0 success
+  */
+
+int main(int argc, char *argv[])
+{
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
+
