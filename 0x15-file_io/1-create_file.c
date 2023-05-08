f27d2b659e7a58c996d69167c34a0b4c4b14c5d6
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-05-08 12:15:45 +0000

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  *create_file - Create a file
+  *@filename: File to be created
+  *@text_content: Content to add to file
+  *
+  *Return: 1 success or -1 otherwise
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_bytes;
+	int len = 0;
+	int flags = O_CREAT | O_WRONLY | O_APPEND;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, flags);
+	write_bytes = write(fd, text_content, len);
+
+	if (fd == -1 || write_bytes == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
