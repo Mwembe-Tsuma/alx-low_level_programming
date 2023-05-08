c2354dc9c70a2cc180ed76a1626b68739ecc61bb
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-05-08 10:27:56 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  *read_textfile - Read from a file and print to stdout
+  *@filenme: File to read from
+  *@letters: Letters to read
+  *
+  *Return: Number of letters read or 0 otherwise
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t read_bytes;
+	ssize_t write_bytes;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	
+	FILE *fp = open(filename, O_RDONLY);
+	if (fp == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	read_bytes = read(fp, buffer, letters);
+	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+
+	free(buffer);
+	close(fp);
+	return (write_bytes);
+}
