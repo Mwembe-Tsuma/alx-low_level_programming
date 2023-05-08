4c58658ae610f3e7d29cbac66f2c21a0afb6ed90
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-05-08 12:48:46 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,25 +10,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_bytes;
+	int fp, write_bytes;
 	int len = 0;
-	int flags = O_WRONLY | O_APPEND;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	fd = open(filename, flags);
-	write_bytes = write(fd, text_content, len);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_bytes = write(fp, text_content, len);
 
-	if (fd == -1 || write_bytes == -1)
+	if (fp == -1 || write_bytes == -1)
 		return (-1);
 
-	close(fd);
+	close(fp);
 	return (1);
 }
