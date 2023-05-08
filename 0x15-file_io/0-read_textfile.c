a6ee64f0c474072f12b91695a7d0c98a0d6f5511
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-05-08 10:40:26 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,16 +15,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t write_bytes;
 	ssize_t fp;
 
-	if (filename == NULL)
-	{
-		return (0);
-	}
+
 	fp = open(filename, O_RDONLY);
 
 	if (fp == -1)
-	{
 		return (0);
-	}
 
 	buffer = malloc(sizeof(char) * letters);
 	read_bytes = read(fp, buffer, letters);
