4ebc430e3ea8f59cb1f5b50ff619d04825f93587
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-19 18:44:41 +0000

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$PWD/lib101-make_me_win.so https://github.com/Mwembe-Tsuma/alx-low_level_programming/0x18-dynamic_libraries/101-make_me_win.so
-export LD_PRELOAD=$PWD/lib101-make_me_win.so
+wget -q --output-document=$PWD/101-make_me_win.so https://github.com/Mwembe-Tsuma/alx-low_level_programming/raw/main/0x18-dynamic_libraries/101-make_me_win.so
+export LD_PRELOAD=$PWD/101-make_me_win.so
