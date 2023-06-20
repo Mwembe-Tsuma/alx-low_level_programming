7e7f1b73135321945cdd3f10c7fefe0ca6df8d2d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-20 06:05:39 +0000

    101-make_me_win

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -5,5 +5,5 @@
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 - 9\n", 17);
-	write(1, "Congratulations, you win the Jackpot!", 37);
+	write(1, "Congratulations, you win the Jackpot!i\n", 38);
 }
