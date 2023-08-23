1939c8f2ece7a968be8e114061a165767822074d
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-08-23 12:02:32 +0000

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,9 +23,15 @@ def island_perimeter(grid):
                 per += 4
 
                 if row > 0 and grid[row - 1][col] == 1:
-                    per -= 2
+                    per -= 1
 
                 if row > 0 and grid[row][col - 1] == 1:
-                    per -= 2
+                    per -= 1
+
+                if row < rows - 1 and grid[row + 1][col] == 1:
+                    per -= 1
+
+                if col < cols - 1 and grid[row][col + 1] == 1:
+                    per -= 1
 
     return per
