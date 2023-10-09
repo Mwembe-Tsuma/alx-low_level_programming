c9d6a2f448f0af535a21f90f6a227ddc774c3f3a
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-09 10:38:06 +0000

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+  *linear_search- Implementation of linear search
+  *@array: The array to search
+  *@size: The size of the array
+  *@value: The value to search
+  *
+  *Return: Value or -1 if value not present or array
+  *is empty
+  *
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
