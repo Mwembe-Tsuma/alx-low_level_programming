b4c6b98e20048959b069c412510a269702958fbb
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-11 06:25:44 +0000

    102-interpolation.c

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <math.h>
+
+
+/**
+  *interpolation_search- Implementation of interpolation search
+  *@array: The array to search
+  *@size: The size of the array
+  *@value: The value to search
+  *
+  *Return: Value or -1 if value not present or array
+  *is empty
+  *
+  */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t pos;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((double)(high - low) /
+					(array[high] - array[low])) * (value - array[low]));
+
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
+	return (-1);
+}
